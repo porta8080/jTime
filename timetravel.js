948df313da14c9539a412653d6a0dd06e14919c5
Porta8080 <adm.porta8080@gmail.com>
2015-08-17 02:30:04 -0300

    stop interval fixed, adding isLeapYear, changing name and adding tt()

--- timetravel.js
+++ timetravel.js
@@ -1,161 +1,170 @@
-//Version 1.0
-//Based on Ruby on Rails' solution
+//Version 1.1
+//Based on the Ruby on Rails' solution
 //Author: Victor Ferreira at Porta8080
 
-function Time(i){
+function TimeTravel(i){
   if(i) this.t = new Date(i);
   else this.t = new Date();
   this.jump = 0;
   this.timer = null;
 }
 
-Time.prototype.toMilliseconds = function(a){
+function tt(i){
+	return new TimeTravel(i);
+}
+
+TimeTravel.prototype.toMilliseconds = function(a){
   if(!a) a = 1;
   return a;
 }
 
-Time.prototype.toSeconds = function(a){
+TimeTravel.prototype.toSeconds = function(a){
   if(!a) a = 1;
   return this.toMilliseconds() * a * 1000;
 }
 
-Time.prototype.toMinutes = function(a){
+TimeTravel.prototype.toMinutes = function(a){
   if(!a) a = 1;
   return this.toSeconds() * a * 60;
 }
 
-Time.prototype.toHours = function(a){
+TimeTravel.prototype.toHours = function(a){
   if(!a) a = 1;
   return this.toMinutes() * a * 60;
 }
 
-Time.prototype.toDays = function(a){
+TimeTravel.prototype.toDays = function(a){
   if(!a) a = 1;
   return this.toHours() * a * 24;
 }
 
-Time.prototype.toWeeks = function(a){
+TimeTravel.prototype.toWeeks = function(a){
   if(!a) a = 1;
   return this.toDays() * a * 7;
 }
 
-Time.prototype.toMonths = function(a){
+TimeTravel.prototype.toMonths = function(a){
   if(!a) a = 1;
   return this.toDays() * a * 30;
 }
 
-Time.prototype.toYears = function(a){
+TimeTravel.prototype.toYears = function(a){
   if(!a) a = 1;
   return this.toDays() * a * 365;
 }
 
-Time.prototype.millisecond = Time.prototype.milliseconds = function(a){
+TimeTravel.prototype.millisecond = TimeTravel.prototype.milliseconds = function(a){
   this.jump = this.toMilliseconds(a);
   return this;
 }
 
-Time.prototype.second = Time.prototype.seconds = function(a){
+TimeTravel.prototype.second = TimeTravel.prototype.seconds = function(a){
   this.jump = this.toSeconds(a);
   return this;
 }
 
-Time.prototype.minute = Time.prototype.minutes = function(a){
+TimeTravel.prototype.minute = TimeTravel.prototype.minutes = function(a){
   this.jump = this.toMinutes(a);
   return this;
 }
 
-Time.prototype.hour = Time.prototype.hours = function(a){
+TimeTravel.prototype.hour = TimeTravel.prototype.hours = function(a){
   this.jump = this.toHours(a);
   return this;
 }
 
-Time.prototype.day = Time.prototype.days = function(a){
+TimeTravel.prototype.day = TimeTravel.prototype.days = function(a){
   this.jump = this.toDays(a);
   return this;
 }
 
-Time.prototype.week = Time.prototype.weeks = function(a){
+TimeTravel.prototype.week = TimeTravel.prototype.weeks = function(a){
   this.jump = this.toWeeks(a);
   return this;
 }
 
-Time.prototype.month = Time.prototype.months = function(a){
+TimeTravel.prototype.month = TimeTravel.prototype.months = function(a){
   this.jump = this.toMonths(a);
   return this;
 }
 
-Time.prototype.year = Time.prototype.years = function(a){
+TimeTravel.prototype.year = TimeTravel.prototype.years = function(a){
   this.jump = this.toYears(a);
   return this;
 }
 
-Time.prototype.add = function(i){
+TimeTravel.prototype.add = function(i){
   var p = i.split(' ');
   this[p[1]](p[0]).after();
   return this;
 }
 
-Time.prototype.subtract = function(i){
+TimeTravel.prototype.subtract = function(i){
   var p = i.split(' ');
   this[p[1]](p[0]).before();
   return this;
 }
 
-Time.prototype.before = function(){
+TimeTravel.prototype.before = function(){
   this.t = new Date(this.t.getTime() - this.jump);
   return this;
 }
 
-Time.prototype.after = function(){
+TimeTravel.prototype.after = function(){
   this.t = new Date(this.t.getTime() + this.jump);
   return this;
 }
 
-Time.prototype.today = function(){
+TimeTravel.prototype.today = function(){
     var t = new Date();
     this.t = new Date(t.getFullYear(),t.getMonth(),t.getDate(),0,0,0,0);
     return this;
 }
 
-Time.prototype.yesterday = function(){
+TimeTravel.prototype.yesterday = function(){
     this.t = new Date(new Date().getTime() - this.toDays(1));
     return this;
 }
 
-Time.prototype.tomorrow = function(){
+TimeTravel.prototype.tomorrow = function(){
     this.t = new Date(new Date().getTime() + this.toDays(1));
     return this;
 }
 
-Time.prototype.now = function(){
+TimeTravel.prototype.now = function(){
     this.t = new Date();
     return this;
 }
 
-Time.prototype.ago = function(){
+TimeTravel.prototype.ago = function(){
     this.t = new Date(new Date().getTime() - this.jump);
     return this;
 }
 
-Time.prototype.fromNow = function(){
+TimeTravel.prototype.fromNow = function(){
     this.t = new Date(new Date().getTime() + this.jump);
     return this;
 }
 
-Time.prototype.lastDayOfMonth = function(){
+TimeTravel.prototype.lastDayOfMonth = function(){
     this.t = new Date(this.t.getFullYear(), this.t.getMonth() + 1, 0);
     return this;
 }
 
-Time.prototype.daysInMonth = function(){
+TimeTravel.prototype.daysInMonth = function(){
     return new Date(this.t.getFullYear(), this.t.getMonth() + 1, 0).getDate();
 }
 
-Time.prototype.get = function(){
+TimeTravel.prototype.isLeapYear = function(){
+	var year = this.t.getFullYear();
+	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+TimeTravel.prototype.get = function(){
     return this.t.getTime();
 }
 
-Time.prototype.format = function(m){
+TimeTravel.prototype.format = function(m){
     var s='';
     var c, t;
 
@@ -207,18 +216,22 @@ Time.prototype.format = function(m){
     return s;
 }
 
-Time.prototype.interval = function(time,callback,params){
+TimeTravel.prototype.interval = function(time,callback,params){
+	var _tt = this;
     var interval = function(time,callback,params){
-        this.timer = setTimeout(function(){
-            var r = callback(params);
-            if(r) params = r;
-            interval(time,callback,params);
-        },time);
+		if(!_tt.stopInterval){
+			_tt.timer = setTimeout(function(){			
+				var r = callback(params);
+				if(r) params = r;
+				interval(time,callback,params);
+			},time);
+		}        
     }
-
+	
+	this.stopInterval = false;
     interval(time,callback,params);
 }
 
-Time.prototype.stop = function(time,callback,params){
-    clearTimeout(this.timer);
+TimeTravel.prototype.stop = function(){
+	this.stopInterval = true;
 }
